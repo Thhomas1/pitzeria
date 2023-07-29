a91b01823fb4a801eeed7300051a9ecc2d7752d2
Thhomas1 <thomasromero10@hotmail.com>
2023-07-29 01:28:40 -0300

    Summary

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,15 @@ import Image from 'next/image'
 const Navbar = () => {
   const user = false
   return (
-    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 lx:px-40'>
         {/* Izq MENU */}
-        <div className="hidden md:flex  gap-4">
+        <div className="hidden md:flex gap-4 flex-1">
           <Link href="/" >Inicio</Link>
-          <Link href="/menu" >Menu</Link>
+          <Link href="/menu">Menu</Link>
           <Link href="/" >Contacto</Link>
         </div>
         {/* LOGO */}
-        <div className='text-xl'>
+        <div className='text-xl md:font-bold flex-1 md:text-center'>
             <Link href="/">
                 PizzEli
             </Link> 
@@ -25,8 +25,8 @@ const Navbar = () => {
             <Menu/>
         </div>
         {/* Der MENU */}
-        <div className="hidden md:flex  gap-4 items-center">
-          <div className="flex items-center gap-2 cursor-pointer bg-orange-200 px-1 rounded-md">
+        <div className="hidden md:flex  gap-4 items-center justify-end flex-1">
+          <div className=" md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-200 px-1 rounded-md">
             <Image src="/phone.png" alt="" width={20} height={20}/>
             <span>123 456 789</span>
           </div>
---
+++ src/components/Slider.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
+
+const data = [
+  {
+    id: 1,
+    title: "Mas crujiente imposible",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "Entregas a todo zona sur",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: " La mejor pizza que puede existir para compartir en familia",
+    image: "/slide3.jpg",
+  },
+];
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+   
+  /* Para la animacion la usamos luego 
+  useEffect(()=>{
+    const interval = setInterval(
+      ()=>setCurrentSlide(prev=>(prev === data.length -1 ? 0 : prev+1))
+    ,2000)
+    return ()=> clearInterval(interval);
+  }, [])
+  */
+// eso es para setear el slider en segundos
+  return (
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-6rem)] lg:flex-row'>
+
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-3 px-6 rounded-md	">Comprar</button>
+      </div>
+      <div className="w-full flex-1 relative">
+        <Image src={data[currentSlide].image} alt='' fill className='object-cover' />
+      </div>
+    </div>
+  )
+}
+// 45:45
+export default Slider
