ab33db28ff4475d5286fa6967bc9ff70c7a1c3d2
Thhomas1 <thomasromero10@hotmail.com>
2023-07-31 19:04:05 -0300

    Offer component

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,9 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div className=''>
+      <div className=""></div>
+      <div className="relative">
+        <Image src="/offerProduct.png" alt='' fill />
+      </div>
+    </div>
   )
 }
-
+// 1:00:05
 export default Offer
