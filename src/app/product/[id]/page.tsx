3f4ce7c9a63c61603ec7659c86208c1a06f2c040
Thhomas1 <thomasromero10@hotmail.com>
2023-08-03 19:46:37 -0300

    calculating price

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -7,7 +7,7 @@ const SingleProductPage = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
       {singleProduct.img && (
-      <div className="relative">
+      <div className="relative w-full h-1/2">
         <Image 
         src={singleProduct.img} 
         alt='' 
@@ -16,8 +16,8 @@ const SingleProductPage = () => {
         />
       </div>
       )}
-      <div className="">
-        <h1>{singleProduct.title}</h1>
+      <div className="h-1/2 flex flex-col gap-4">
+        <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
         {/*<p>{singleProduct.price}</p>*/}
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 
 type Props = {
     price:number;
@@ -6,11 +8,40 @@ type Props = {
     options?: {title: string; additionalPrice: number}[]
 }
 const Price = ({price,id,options}: Props) => {
+  const [total,setTotal] = useState(price);
+  const [quantity,setQuantity] = useState(1);
+  const [selected,setSelected] = useState(0);
+  
   return (
-    <div>
-        <h2></h2>
+    <div className='flex flex-col gap-4'>
+        <h2 className='text-2xl font-bold'>${total.toFixed(2)}</h2>
+        <div className="flex gap-4">
+          {options?.map((option,index)=>(
+            <button 
+            key={option.title} 
+            className='min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md'
+            style={{
+              background: selected === index ? "rgb(255,183,3)" : "white",
+              color: selected === index ? "white" : "red"
+            }}
+            onClick={()=>setSelected(index)}
+            >{option.title}</button>
+          ))}
+        </div>
+        <div className="flex justify-between items-center">
+
+          <div className=" flex justify-between w-full p-3 ring-1 ring-red-400">
+            <span>Cantidad</span>
+            <div className='flex gap-4 items-center'>
+              <button>{'<'}</button>
+              <span>{quantity}</span>
+              <button>{'>'}</button>
+            </div>
+          </div>
+          <button className='uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500 rounded-sm'>Agregar</button>
+        </div>
     </div>
   )
 }
-// 1:28
+// 1:37:55
 export default Price
