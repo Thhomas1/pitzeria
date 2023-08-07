d84f3c018e5ba47f77fcc527bedb70e52455fd4f
Thhomas1 <thomasromero10@hotmail.com>
2023-08-06 23:17:36 -0300

    cartpagefinished

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,25 +3,48 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500">
-      <div className="h-1/2 p-4">
-        <div className="">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+        <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100}/>
           <div className="">
-            <h1>Siciliana</h1>
+            <h1 className='uppercase text-xl font-bold'>Siciliana</h1>
             <span>Large</span>
           </div>
-          <h2>$100.00</h2>
-          <span>X</span>
+          <h2 className='font-bold'>$100.00</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100}/>
+          <div className="">
+            <h1 className='uppercase text-xl font-bold'>Siciliana</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold'>$100.00</h2>
+          <span className='cursor-pointer'>X</span>
         </div>
       </div>
-      <div className="h-1/2 p-4 bg-fuchsia-100">
-        <div className="">
+      <div className="h-1/2 p-4 bg-fuchsia-100 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className="flex justify-between ">
           <span className="">Subtotal (3 item)</span>
           <span className="">$80.00</span>
         </div>
+        <div className="flex justify-between ">
+          <span className="">Service Cost</span>
+          <span className="">$0.00</span>
+        </div>
+        <div className="flex justify-between ">
+          <span className="">Delivery Cost</span>
+          <span className="text-green-500">FREE!</span>
+        </div>
+        <hr className="my-2" />
+        <div className="flex justify-between ">
+          <span className="">TOTAL(INCL. VAT)</span>
+          <span className="font-bold">$81.80</span>
+        </div>
+        
         <hr className='' />
-        <button className="bg-red-500 text-white p-3 rounded-md w-1/2">Paga</button>
+        <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">Pagar</button>
       </div>
     </div>
   )
